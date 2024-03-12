a853d1671a60ef5039375023b49aa37ae8be9524
ZeRoX <imrek93@gmail.com>
2024-03-12 20:50:57 +0100

    All solo build for now

--- helldiver2_mode.py
+++ helldiver2_mode.py
@@ -17,19 +17,21 @@ def update_screen(splash, macro_name, display):
     # Remove the macro label after 3 seconds
     splash.remove(macro_label)
     display.refresh()
-
+    
 def handle_keypress(key, cc, write_text, keyboard, splash, display ):
-
+    
     # Macro names or actions
     macro_names = {
         0: "Reinforce",
         1: "Resupply",
-        2: "Open Chrome",
-        3: "Volume Up",
-        4: "Grab",
+        11: "Eagle Airstrike",
+        12: "Orbital Precision",
+        13: "Grenade Launcher",
+        14: "Guard Dog Laser",
         # Add more macro names and their corresponding keys as needed
     }
     
+    #Reinforce
     if key[0].value:
         keyboard.press(Keycode.CONTROL)
         time.sleep(0.05)
@@ -63,8 +65,8 @@ def handle_keypress(key, cc, write_text, keyboard, splash, display ):
         time.sleep(0.05)
 
         update_screen(splash, macro_names[0], display)
-        
-        
+
+    #Resupply    
     if key[1].value:
         keyboard.press(Keycode.CONTROL)
         time.sleep(0.05)
@@ -88,27 +90,135 @@ def handle_keypress(key, cc, write_text, keyboard, splash, display ):
         time.sleep(0.05)
         keyboard.release(Keycode.RIGHT_ARROW)
         time.sleep(0.05)
-        
+
         keyboard.release(Keycode.CONTROL)
         time.sleep(0.05)
         update_screen(splash, macro_names[1], display)
-        
-    if key[2].value:
-        keyboard.send(Keycode.GUI)
-        time.sleep(0.4)
-        write_text.write('chrome\n')
-        time.sleep(0.2)
-        write_text.write('\n')
-        time.sleep(1)
-        write_text.write('https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1\n')
-    
-    if key[3].value:
-        cc.send(ConsumerControlCode.VOLUME_INCREMENT)
-        time.sleep(0.2)
-    
-    if key[4].value:
-        print("test")
-        keyboard.send(Keycode.N)
-        time.sleep(0.3)
 
+    #Eagle Airstrike    
+    if key[11].value:
+        keyboard.press(Keycode.CONTROL)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.UP_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.UP_ARROW)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.RIGHT_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.RIGHT_ARROW)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.DOWN_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.DOWN_ARROW)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.RIGHT_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.RIGHT_ARROW)
+        time.sleep(0.05)
+
+        keyboard.release(Keycode.CONTROL)
+        time.sleep(0.05)
+        update_screen(splash, macro_names[11], display)
+    
+    #Orbital Precision Strike
+    if key[12].value:
+        keyboard.press(Keycode.CONTROL)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.LEFT_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.LEFT_ARROW)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.LEFT_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.LEFT_ARROW)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.UP_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.UP_ARROW)
+        time.sleep(0.05)
+
+        keyboard.release(Keycode.CONTROL)
+        time.sleep(0.05)
+        update_screen(splash, macro_names[12], display)
+    
+    #Granade Launcher
+    if key[13].value:
+        keyboard.press(Keycode.CONTROL)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.DOWN_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.DOWN_ARROW)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.LEFT_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.LEFT_ARROW)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.UP_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.UP_ARROW)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.LEFT_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.LEFT_ARROW)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.DOWN_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.DOWN_ARROW)
+        time.sleep(0.05)
+
+        keyboard.release(Keycode.CONTROL)
+        time.sleep(0.05)
+        update_screen(splash, macro_names[13], display)
+
+    #AX/LAS-5 "Guard Dog" Rover
+    if key[14].value:
+        keyboard.press(Keycode.CONTROL)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.DOWN_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.DOWN_ARROW)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.UP_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.UP_ARROW)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.LEFT_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.LEFT_ARROW)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.UP_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.UP_ARROW)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.RIGHT_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.RIGHT_ARROW)
+        time.sleep(0.05)
+
+        keyboard.press(Keycode.RIGHT_ARROW)
+        time.sleep(0.05)
+        keyboard.release(Keycode.RIGHT_ARROW)
+        time.sleep(0.05)
+
+        keyboard.release(Keycode.CONTROL)
+        time.sleep(0.05)
+        update_screen(splash, macro_names[14], display)
+        
     time.sleep(0.0001)
